a8a1d9e5ef24094424ad08e2a88ef8c530d66c29
fxbin <fxbin123@gmail.com>
2021-08-04 18:39:34 +0800

    feat(all): 🎉  project init...

---
+++ src/main/java/cn/fxbin/swagger/autoconfigure/web/SwaggerWebAutoConfiguration.java
@@ -0,0 +1,42 @@
+package cn.fxbin.swagger.autoconfigure.web;
+
+import cn.fxbin.swagger.autoconfigure.SwaggerProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.swagger.configuration.SwaggerCommonConfiguration;
+import springfox.documentation.swagger2.configuration.Swagger2WebMvcConfiguration;
+import springfox.documentation.swagger2.web.Swagger2ControllerWebMvc;
+
+import static cn.fxbin.swagger.autoconfigure.SwaggerProperties.SPRING_SWAGGER_PREFIX;
+
+/**
+ * SwaggerWebAutoConfiguration
+ *
+ * @author fxbin
+ * @version v1.0
+ * @since 2020/3/31 18:54
+ */
+@Configuration(
+        proxyBeanMethods = false
+)
+@EnableConfigurationProperties(SwaggerProperties.class)
+@ConditionalOnWebApplication(type = Type.SERVLET)
+@ConditionalOnClass({SwaggerCommonConfiguration.class, Swagger2WebMvcConfiguration.class, Swagger2ControllerWebMvc.class})
+@ConditionalOnProperty(prefix = SPRING_SWAGGER_PREFIX, name = "enabled", havingValue = "true")
+public class SwaggerWebAutoConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("doc.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+}
---
+++ src/main/java/cn/fxbin/swagger/autoconfigure/webflux/SwaggerWebFluxAutoConfiguration.java
@@ -0,0 +1,32 @@
+package cn.fxbin.swagger.autoconfigure.webflux;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.ResourceHandlerRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+import static cn.fxbin.swagger.autoconfigure.SwaggerProperties.SPRING_SWAGGER_PREFIX;
+
+/**
+ * SwaggerWebFluxAutoConfiguration
+ *
+ * @author fxbin
+ * @version v1.0
+ * @since 2021/4/28 11:50
+ */
+@Configuration(
+        proxyBeanMethods = false
+)
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
+@ConditionalOnProperty(prefix = SPRING_SWAGGER_PREFIX, name = "enabled", havingValue = "true")
+public class SwaggerWebFluxAutoConfiguration implements WebFluxConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
+                .resourceChain(false);
+    }
+
+}
